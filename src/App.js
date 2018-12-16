8806f44ce39f1f49f546091e7fe9207a93e73399
spennino <spennino@velocitypartners.net>
2018-12-16 20:58:10 -0300

    RT03: Static (pure react) and without CSS version of the idea

--- src/App.js
+++ src/App.js
@@ -1,25 +1,149 @@
 import React, { Component } from 'react'
-import logo from './logo.svg'
 import './App.css'
+
+class FilteredRow extends React.Component {
+  render () {
+    const data = this.props.data
+
+    return (
+      <tr>
+        <td>{data.name}</td>
+        <td>{data.position}</td>
+        <td>{data.team}</td>
+        <td>{data.age}</td>
+      </tr>
+    )
+  }
+}
+
+class FilteredTable extends React.Component {
+  render () {
+    const rows = []
+    const categories = []
+
+    this.props.categories.forEach((el, idx) => {
+      categories.push(
+        <th key={el}>{el}</th>
+      )
+    })
+
+    this.props.results.forEach((el, idx) => {
+      rows.push(
+        <FilteredRow
+          data={el}
+          key={el.name} />
+      )
+    })
+
+    return (
+      <table className='FilteredTable'>
+        <thead>
+          <tr>
+            {categories}
+          </tr>
+        </thead>
+        <tbody>
+          {rows}
+        </tbody>
+      </table>
+    )
+  }
+}
+
+class SearchInput extends React.Component {
+  constructor () {
+    super()
+    this.state = {
+      value: ''
+    }
+    this.validRE = {
+      onlyLetters: /^[A-Za-z]+$/,
+      onlyNumbers: /^[0-9\b]+$/
+    }
+  }
+
+  correctInputText (e) {
+    const re = this.validRE[this.props.restrict]
+    if (e.target.value === '' || (re && re.test(e.target.value))) {
+      this.setState({value: e.target.value})
+    }
+  }
+
+  render () {
+    return (
+      <input type='text' placeholder={this.props.placeholder} value={this.state.value} onChange={e => this.correctInputText(e)} />
+    )
+  }
+}
+
+class SearchListInput extends React.Component {
+  render () {
+    const opt = []
+    this.props.list.forEach(el => {
+      opt.push(
+        <option key={el.val} value={el.val}>{el.name}</option>
+      )
+    })
+    return (
+      <select>
+        { this.props.initialVal ? <option value='' defaultValue>{this.props.initialVal}</option> : '' }
+        {opt}
+      </select>
+    )
+  }
+}
+
+const VALID_POSITIONS = [
+  { val: 'AM', name: 'Attacking Midfield' },
+  { val: 'CM', name: 'Central Midfield' },
+  { val: 'CB', name: 'Centre-Back' },
+  { val: 'CF', name: 'Centre-Forward' },
+  { val: 'DM', name: 'Defensive Midfield' },
+  { val: 'GK', name: 'Keeper' },
+  { val: 'LM', name: 'Left Midfield' },
+  { val: 'LW', name: 'Left Wing' },
+  { val: 'LB', name: 'Left-Back' },
+  { val: 'RB', name: 'Right-Back' }
+]
+
+class SearchBar extends React.Component {
+  render () {
+    return (
+      <form>
+        <SearchInput placeholder='Player Name' restrict='onlyLetters' />
+        <SearchListInput list={VALID_POSITIONS} initialVal='Position' />
+        <SearchInput placeholder='Age' restrict='onlyNumbers' />
+        <button>Search</button>
+      </form>
+    )
+  }
+}
+
+class FilterableTable extends React.Component {
+  render () {
+    return (
+      <div>
+        <SearchBar />
+        <FilteredTable results={this.props.results} categories={['Player', 'Position', 'Team', 'Age']} />
+      </div>
+    )
+  }
+}
+
+const PLAYERS = [
+  {team: 'Barcelona', age: 30, position: 'Forward', name: 'Lionel Messi'},
+  {team: 'Barcelona', age: 35, position: 'Defender', name: 'Javier Mascherano'},
+  {team: 'Real Madrid', age: 30, position: 'Forward', name: 'Cristiano Ronaldo'},
+  {team: 'Real Madrid', age: 22, position: 'Defensive Midfield', name: 'Sergio Reguilón'},
+  {team: 'Real Madrid', age: 22, position: 'Central Midfield', name: 'Dani Ceballos'},
+  {team: 'Atlético Madrid', age: 23, position: 'Forward', name: 'Ángel Correa'}
+]
 
 class App extends Component {
   render () {
     return (
       <div className='App'>
-        <header className='App-header'>
-          <img src={logo} className='App-logo' alt='logo' />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className='App-link'
-            href='https://reactjs.org'
-            target='_blank'
-            rel='noopener noreferrer'
-          >
-            Learn React
-          </a>
-        </header>
+        <FilterableTable results={PLAYERS} />
       </div>
     )
   }
