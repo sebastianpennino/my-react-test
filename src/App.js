0f8076e6d11749efb1d91764552b1b10124bf724
spennino <spennino@velocitypartners.net>
2018-12-18 23:06:23 -0300

    RT04: Added states, separated into smart and dumb components

--- src/App.js
+++ src/App.js
@@ -1,130 +1,137 @@
 import React, { Component } from 'react'
 import './App.css'
 
-class FilteredRow extends React.Component {
-  render () {
-    const data = this.props.data
+const FilteredRow = props =>
+  <tr className='filteredRow'>
+    <td>{props.data.name}</td>
+    <td>{props.data.position}</td>
+    <td>{props.data.team}</td>
+    <td>{props.data.age}</td>
+  </tr>
 
-    return (
-      <tr>
-        <td>{data.name}</td>
-        <td>{data.position}</td>
-        <td>{data.team}</td>
-        <td>{data.age}</td>
-      </tr>
-    )
-  }
+const FilteredTable = props => {
+  const categories = props.categories.map(el => <th key={el}>{el}</th>)
+  const results = props.results.map(el => <FilteredRow data={el} key={el.name} />)
+
+  return (
+    <table className='filteredTable'>
+      <thead>
+        <tr>
+          {categories}
+        </tr>
+      </thead>
+      <tbody>
+        {results}
+      </tbody>
+    </table>
+  )
 }
 
-class FilteredTable extends React.Component {
-  render () {
-    const rows = []
-    const categories = []
+const SearchInput = props =>
+  <input className='SearchInput' type='text' placeholder={props.placeholder} value={props.value} onChange={props.changeFn} />
 
-    this.props.categories.forEach((el, idx) => {
-      categories.push(
-        <th key={el}>{el}</th>
-      )
-    })
-
-    this.props.results.forEach((el, idx) => {
-      rows.push(
-        <FilteredRow
-          data={el}
-          key={el.name} />
-      )
-    })
-
-    return (
-      <table className='FilteredTable'>
-        <thead>
-          <tr>
-            {categories}
-          </tr>
-        </thead>
-        <tbody>
-          {rows}
-        </tbody>
-      </table>
-    )
-  }
+const SearchListInput = props => {
+  const options = props.list.map(el => <option key={el.val} value={el.name}>{el.name} ({el.val})</option>)
+  return (
+    <select className='searchListInput' onChange={props.changeFn} >
+      { props.initialVal ? <option value='' defaultValue>{props.initialVal}</option> : '' }
+      { options }
+    </select>
+  )
 }
 
-class SearchInput extends React.Component {
-  constructor () {
-    super()
-    this.state = {
-      value: ''
-    }
-    this.validRE = {
-      onlyLetters: /^[A-Za-z]+$/,
-      onlyNumbers: /^[0-9\b]+$/
-    }
-  }
-
-  correctInputText (e) {
-    const re = this.validRE[this.props.restrict]
-    if (e.target.value === '' || (re && re.test(e.target.value))) {
-      this.setState({value: e.target.value})
-    }
-  }
-
-  render () {
-    return (
-      <input type='text' placeholder={this.props.placeholder} value={this.state.value} onChange={e => this.correctInputText(e)} />
-    )
-  }
-}
-
-class SearchListInput extends React.Component {
-  render () {
-    const opt = []
-    this.props.list.forEach(el => {
-      opt.push(
-        <option key={el.val} value={el.val}>{el.name}</option>
-      )
-    })
-    return (
-      <select>
-        { this.props.initialVal ? <option value='' defaultValue>{this.props.initialVal}</option> : '' }
-        {opt}
-      </select>
-    )
-  }
-}
-
-const VALID_POSITIONS = [
-  { val: 'AM', name: 'Attacking Midfield' },
-  { val: 'CM', name: 'Central Midfield' },
-  { val: 'CB', name: 'Centre-Back' },
-  { val: 'CF', name: 'Centre-Forward' },
-  { val: 'DM', name: 'Defensive Midfield' },
-  { val: 'GK', name: 'Keeper' },
-  { val: 'LM', name: 'Left Midfield' },
-  { val: 'LW', name: 'Left Wing' },
-  { val: 'LB', name: 'Left-Back' },
-  { val: 'RB', name: 'Right-Back' }
-]
-
-class SearchBar extends React.Component {
-  render () {
-    return (
-      <form>
-        <SearchInput placeholder='Player Name' restrict='onlyLetters' />
-        <SearchListInput list={VALID_POSITIONS} initialVal='Position' />
-        <SearchInput placeholder='Age' restrict='onlyNumbers' />
-        <button>Search</button>
-      </form>
-    )
-  }
-}
+const SearchBar = props =>
+  <form className='searchBar'>
+    <SearchInput placeholder='Player Name' val={props.filterName} changeFn={props.onChangeFilterNameFn} />
+    <SearchListInput list={props.positions} initialVal='Position' val={props.filterPosition} changeFn={props.onChangeFilterPositionFn} />
+    <SearchInput placeholder='Age' val={props.filterAge} changeFn={props.onChangeFilterAgeFn} />
+    <button onClick={props.onSearchFn}>Search</button>
+  </form>
 
 class FilterableTable extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      filterName: '',
+      filterPosition: '',
+      filterAge: '',
+      results: [],
+      categories: [],
+      positions: [],
+      filteredResults: [],
+      counter: 0
+    }
+
+    // This binding is necessary to make 'this' work in the callback
+    this.updateFilterName = this.updateFilterName.bind(this)
+    this.updateFilterPosition = this.updateFilterPosition.bind(this)
+    this.updateFilterAge = this.updateFilterAge.bind(this)
+    this.filterResults = this.filterResults.bind(this)
+  }
+
+  componentDidMount () {
+    fetchSomeCategories(categories => this.setState({ categories }))
+    fetchSomePlayers(results =>
+      this.setState({ results, filteredResults: results }
+      )
+    )
+    fetchSomePositions(positions => this.setState({ positions }))
+  }
+
+  comparator (data, filterName, filterPosition, filterAge) {
+    return data.filter(el => {
+      return (filterName && el.name.indexOf(filterName) !== -1) || (filterPosition && el.position === filterPosition) || (filterAge && el.age === parseInt(filterAge, 10))
+    })
+  }
+
+  filterResults (e) {
+    e.preventDefault()
+    this.setState(prevState => {
+      const newResults = this.comparator(prevState.results, prevState.filterName, prevState.filterPosition, prevState.filterAge)
+      return newResults.length > 0 ? { filteredResults: newResults } : { filteredResults: prevState.results }
+    })
+  }
+
+  updateFilterName (e) {
+    // const onlyLetters = /^[A-Za-z]+$/
+    const newVal = e.target.value
+    this.setState(prevState => {
+      return { filterName: newVal }
+    })
+  }
+
+  updateFilterPosition (e) {
+    const newVal = e.target.value
+    this.setState(prevState => {
+      return { filterPosition: newVal }
+    })
+  }
+
+  updateFilterAge (e) {
+    // const onlyNumbers = /^[0-9\b]+$/
+    const newVal = e.target.value
+    this.setState(prevState => {
+      return { filterAge: newVal }
+    })
+  }
+
   render () {
     return (
       <div>
-        <SearchBar />
-        <FilteredTable results={this.props.results} categories={['Player', 'Position', 'Team', 'Age']} />
+        <SearchBar
+          filterName={this.state.filterName}
+          filterPosition={this.state.filterPosition}
+          filterAge={this.state.filterAge}
+          positions={this.state.positions}
+          onSearchFn={this.filterResults}
+          onChangeFilterNameFn={this.updateFilterName}
+          onChangeFilterPositionFn={this.updateFilterPosition}
+          onChangeFilterAgeFn={this.updateFilterAge}
+        />
+        <FilteredTable
+          results={this.state.filteredResults}
+          categories={this.state.categories}
+        />
       </div>
     )
   }
@@ -139,11 +146,33 @@ const PLAYERS = [
   {team: 'Atlético Madrid', age: 23, position: 'Forward', name: 'Ángel Correa'}
 ]
 
+const VALID_POSITIONS = [
+  { val: 'AM', name: 'Attacking Midfield' },
+  { val: 'CM', name: 'Central Midfield' },
+  { val: 'CB', name: 'Centre-Back' },
+  { val: 'CF', name: 'Centre-Forward' },
+  { val: 'DM', name: 'Defensive Midfield' },
+  { val: 'GK', name: 'Keeper' },
+  { val: 'LM', name: 'Left Midfield' },
+  { val: 'LW', name: 'Left Wing' },
+  { val: 'LB', name: 'Left-Back' },
+  { val: 'RB', name: 'Right-Back' }
+]
+
+const CATEGORIES = [
+  'Player', 'Position', 'Team', 'Age'
+]
+
+// These are just a placeholder for the real requests
+const fetchSomeCategories = cb => cb(CATEGORIES)
+const fetchSomePlayers = cb => cb(PLAYERS)
+const fetchSomePositions = cb => cb(VALID_POSITIONS)
+
 class App extends Component {
   render () {
     return (
       <div className='App'>
-        <FilterableTable results={PLAYERS} />
+        <FilterableTable />
       </div>
     )
   }
